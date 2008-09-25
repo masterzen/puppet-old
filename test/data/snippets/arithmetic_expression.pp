df706f145359760b52047ea9a0a920d7d9c20cee
Brice Figureau <brice-puppet@daysofwonder.com>
2008-09-25 20:53:02 +0200

    add complex lexer/parser test

--- test/data/snippets/arithmetic_expression.pp
+++ test/data/snippets/arithmetic_expression.pp
@@ -2,7 +2,7 @@
 $one = 1.30
 $two = 2.034e-2
 
-$result = ((( 1e2 + 2) / 3) + 4 * 5.45) - (6 << 7) + (0x800 + -9)
+$result = ((( $two + 2) / $one) + 4 * 5.45) - (6 << 7) + (0x800 + -9)
 
 
-notice("result is $result")
+notice("result is $result == 1295.87692307692")
