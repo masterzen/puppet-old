puppetlabs/puppet

commit a30ecf2aeffd71960bd806fb28cd6d1b8adc2452

Make fileserver use fileset for recursion and handle dangling links by ignoring them fixing #1544

lib/puppet/network/handler/fileserver.rb
@@ -651,55 +651,38 @@ class Puppet::Network::Handler
             # and "bad batch".
             #
             def list(relpath, recurse, ignore, client = nil)
-                reclist(file_path(relpath, client), nil, recurse, ignore)
+                require 'puppet/file_serving'
+                require 'puppet/file_serving/fileset'
+                abspath = file_path(relpath, client)
+                if FileTest.exists?(abspath)
+                    if FileTest.directory?(abspath) and recurse
+                        return reclist(abspath, recurse, ignore)
+                    else
+                        return [["/", File.stat(abspath).ftype]]
+                    end
+                end
+                return nil
             end
 
-            # Recursively list the files in this tree.
-            def reclist(basepath, abspath, recurse, ignore)
-                abspath = basepath if abspath.nil?
-                relpath = abspath.sub(%r{^#{basepath}}, '')
-                relpath = "/#{relpath}" if relpath[0] != ?/  #/
-                
-                return unless FileTest.exists?(abspath)
-                
-                desc = [relpath]
-                
-                ftype = File.stat(abspath).ftype
-
-                desc << ftype
-                if recurse.is_a?(Integer)
-                    recurse -= 1
-                end
-
-                ary = [desc]
-                if recurse == true or (recurse.is_a?(Integer) and recurse > -1)
-                    if ftype == "directory"
-                        children = Dir.entries(abspath)
-                        if ignore
-                            children = handleignore(children, abspath, ignore)
-                        end  
-                        children.each { |child|
-                            next if child =~ /^\.\.?$/
-                            reclist(basepath, File.join(abspath, child), recurse, ignore).each { |cobj|
-                                ary << cobj
-                            }
-                        }
+            def reclist(abspath, recurse, ignore)
+                args = { :recurse => recurse, :links => :follow }
+                args[:ignore] = ignore if ignore
+                fs = Puppet::FileServing::Fileset.new(abspath, args)
+                ary = fs.files.collect do |file|
+                    if file == "."
+                        file = "/"
+                    else
+                        file = File.join("/", file )
                     end
+                    stat = fs.stat(File.join(abspath, file))
+                    next if stat.nil?
+                    [ file, stat.ftype ]
                 end
 
                 return ary.compact
             end
 
-            # Deal with ignore parameters.
-            def handleignore(files, path, ignore_patterns)
-                ignore_patterns.each do |ignore|
-                    files.delete_if do |entry|
-                        File.fnmatch(ignore, entry, File::FNM_DOTMATCH)
-                    end
-                end
-                return files
-            end
-        end  
+        end
 
         # A special mount class specifically for the plugins mount -- just
         # has some magic to effectively do a union mount of the 'plugins'

lib/puppet/file_serving/fileset.rb
@@ -120,7 +120,7 @@ class Puppet::FileServing::Fileset
 
         return result
     end
-
+    public
     # Stat a given file, using the links-appropriate method.
     def stat(path)
         unless defined?(@stat_method)

masterzen/puppet-old

commit fb13f3e8bec665aeb5d32368f80db654761bbf65

Fix several small regressions in plugins mount
Since a30ecf2aeffd71960bd806fb28cd6d1b8adc2452, reclist has one parameter
less, but Puppet::Network::Handler::PluginMount.list wasn't ported to the
new API.

While mounting plugins, reclist wasn't requiring 'file_serving/fileset',
leading to an NameError.

The change to the new API meant that we lost the existance test of plugins
mount directory. It was failing when the client was mounting module plugins that
weren't existing (like facter and no custom facts defined).

Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

lib/puppet/network/handler/fileserver.rb
@@ -653,8 +653,6 @@ class Puppet::Network::Handler
             # and "bad batch".
             #
             def list(relpath, recurse, ignore, client = nil)
-                require 'puppet/file_serving'
-                require 'puppet/file_serving/fileset'
                 abspath = file_path(relpath, client)
                 if FileTest.exists?(abspath)
                     if FileTest.directory?(abspath) and recurse
@@ -667,6 +665,8 @@ class Puppet::Network::Handler
             end
 
             def reclist(abspath, recurse, ignore)
+                require 'puppet/file_serving'
+                require 'puppet/file_serving/fileset'
                 args = { :recurse => recurse, :links => :follow }
                 args[:ignore] = ignore if ignore
                 fs = Puppet::FileServing::Fileset.new(abspath, args)
@@ -720,9 +720,12 @@ class Puppet::Network::Handler
             def list(relpath, recurse, ignore, client = nil)
                 result = []
                 valid_modules.each do |m|
-                    ary = reclist(mod_file_path(m, relpath, client), nil, recurse, ignore)
-                    ary = [] if ary.nil?
-                   result += ary
+                    modpath = mod_file_path(m, relpath, client)
+                    if FileTest.exists?(modpath)
+                        ary = reclist(modpath, recurse, ignore)
+                        ary = [] if ary.nil?
+                        result += ary
+                    end
                 end
                 result
             end

spec/unit/network/handler/fileserver.rb
@@ -126,6 +126,44 @@ describe Puppet::Network::Handler::FileServer do
         list.sort.should == [ ["/aFile", "file"], ["/", "directory"] ].sort
     end
 
+    describe Puppet::Network::Handler::FileServer::PluginMount do
+        PLUGINS = Puppet::Network::Handler::FileServer::PLUGINS
+
+        # create a module plugin hierarchy
+        def create_plugin(mod, plugin)
+            dirname = File.join(@basedir, mod)
+            Dir.mkdir(dirname)
+            plugins = File.join(dirname, PLUGINS)
+            Dir.mkdir(plugins)
+            facter = File.join(plugins, plugin)
+            Dir.mkdir(facter)
+            create_file(File.join(facter,"fact.rb"))
+        end
+
+        before :each do
+            @modules = ["one","two"]
+            Puppet::Module.stubs(:all).returns(@modules.collect{ |p| File.join(@basedir,p)} )
+            @modules.each { |m| create_plugin(m, "facter") }
+
+            @modules.each do |p|
+                File.stubs(:directory?).with(File.join(@basedir,p,PLUGINS)).returns(true)
+            end
+
+            @mount = Puppet::Network::Handler::FileServer::PluginMount.new(PLUGINS)
+            @mount.allow("*")
+        end
+        
+        it "should return a merged view of all plugins for all modules" do
+            list = @mount.list("facter",true,false)
+            list.should == [["/", "directory"], ["/fact.rb", "file"], ["/", "directory"], ["/fact.rb", "file"]]
+        end
+
+        it "should not fail for inexistant plugins type" do
+            lambda { @mount.list("puppet/parser",true,false) }.should_not raise_error
+        end
+
+    end
+
     after do
         FileUtils.rm_rf(@basedir)
     end