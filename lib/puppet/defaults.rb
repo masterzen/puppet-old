3e814ac72f523dbd185da704f793348557810d77
Brice Figureau <brice-puppet@daysofwonder.com>
2009-09-13 16:24:50 +0200

    Storeconfigs settings initialize storeconfigs proxy
    Signed-off-by: Brice Figureau <brice-puppet@daysofwonder.com>

--- lib/puppet/defaults.rb
+++ lib/puppet/defaults.rb
@@ -752,6 +752,7 @@ module Puppet
                 require 'puppet/resource/catalog'
                 if value
                     raise "StoreConfigs not supported without ActiveRecord 2.1 or higher" unless Puppet.features.rails?
+                    Puppet::Storeconfigs.source = :rails
                     Puppet::Resource::Catalog.cache_class = :active_record unless Puppet.settings[:async_storeconfigs]
                     Puppet::Node::Facts.cache_class = :active_record
                     Puppet::Node.cache_class = :active_record
